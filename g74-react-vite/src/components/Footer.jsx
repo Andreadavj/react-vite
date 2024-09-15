3547d147418376535bbeeb7ebe2d802c2f07ca5e
Doris Andrea Valverde Jara <andrea.avj03@gmail.com>
2024-09-15 19:14:29 -0300

    react

---
+++ g74-react-vite/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <footer className="footer bg-dark text-white text-center py-3">
+        <p>
+          © 2024 - Pizzería Mamma Mia! - Todos los derechos reservados <i class="fa-solid fa-pizza-slice"></i>{" "}
+        </p>
+      </footer>
+    );
+  };
+  
+  export default Footer;

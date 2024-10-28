6a0f1b419d3767a8edd53426890ea9174ecabcda
Doris Andrea Valverde Jara <andrea.avj03@gmail.com>
2024-10-28 00:58:09 -0300

    m

--- g74-react-vite/src/components/Navbar.jsx
+++ g74-react-vite/src/components/Navbar.jsx
@@ -1,113 +1,64 @@
-// import React from "react";
+import React from "react";
 
-// const Navbar = () => {
-//   const total = 25000;
-//   const token = false;
+const Navbar = () => {
+  const total = 25000;
+  const token = false;
 
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-//       <div className="container-fluid">
-//         <a className="navbar-brand" href="#">
-//           Pizzería Mamma Mia!
-//         </a>
-//         <button
-//           className="navbar-toggler"
-//           type="button"
-//           data-bs-toggle="collapse"
-//           data-bs-target="#navbarNav"
-//           aria-controls="navbarNav"
-//           aria-expanded="false"
-//           aria-label="Toggle navigation"
-//         >
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarNav">
-//           <div className="navbar-nav">
-//             <a
-//               className="nav-link btn btn-outline-success me-2"
-//               aria-current="page"
-//               href="#"
-//             >
-//               Home 🍕
-//             </a>
-//             {token ? (
-//               <>
-//                 <a className="nav-link btn btn-outline-success me-2" href="#">
-//                   Profile 👨‍💻
-//                 </a>
-//                 <a className="nav-link btn btn-outline-success me-2" href="#">
-//                   Logout 🔐
-//                 </a>
-//               </>
-//             ) : (
-//               <>
-//                 <a className="nav-link btn btn-outline-success me-2" href="#">
-//                   Login 🚪
-//                 </a>
-//                 <a className="nav-link btn btn-outline-success me-2" href="#">
-//                   Register 🔐
-//                 </a>
-//               </>
-//             )}
-//           </div>
-//           <div className="navbar-nav ms-auto">
-//             <a className="nav-link btn btn-outline-success" href="#">
-//               🛒 Total: ${total.toLocaleString()}
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      Pizzería Mamma Mia!
-    </a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <div className="navbar-nav"> {/* Aquí aplica display: flex */}
-        <a className="nav-link btn btn-outline-success me-2" href="#">
-          Home 🍕
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Pizzería Mamma Mia!
         </a>
-        {token ? (
-          <>
-            <a className="nav-link btn btn-outline-success me-2" href="#">
-              Profile 👨‍💻
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav">
+            <a
+              className="nav-link btn btn-outline-success me-2"
+              aria-current="page"
+              href="#"
+            >
+              Home 🍕
             </a>
-            <a className="nav-link btn btn-outline-success me-2" href="#">
-              Logout 🔐
+            {token ? (
+              <>
+                <a className="nav-link btn btn-outline-success me-2" href="#">
+                  Profile 👨‍💻
+                </a>
+                <a className="nav-link btn btn-outline-success me-2" href="#">
+                  Logout 🔐
+                </a>
+              </>
+            ) : (
+              <>
+                <a className="nav-link btn btn-outline-success me-2" href="#">
+                  Login 🚪
+                </a>
+                <a className="nav-link btn btn-outline-success me-2" href="#">
+                  Register 🔐
+                </a>
+              </>
+            )}
+          </div>
+          <div className="navbar-nav ms-auto">
+            <a className="nav-link btn btn-outline-success" href="#">
+              🛒 Total: ${total.toLocaleString()}
             </a>
-          </>
-        ) : (
-          <>
-            <a className="nav-link btn btn-outline-success me-2" href="#">
-              Login 🚪
-            </a>
-            <a className="nav-link btn btn-outline-success me-2" href="#">
-              Register 🔐
-            </a>
-          </>
-        )}
+          </div>
+        </div>
       </div>
-      <div className="navbar-nav ms-auto"> {/* Este también debe estar con flex */}
-        <a className="nav-link btn btn-outline-success" href="#">
-          🛒 Total: ${total.toLocaleString()}
-        </a>
-      </div>
-    </div>
-  </div>
-</nav>
+    </nav>
+  );
+};
+
+export default Navbar;
